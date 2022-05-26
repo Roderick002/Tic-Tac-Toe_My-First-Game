1c00e357a95aeeb86b64c2f7230ba1b3421e52e9
Roderick002 <ballaranroderick@gmail.com>
2022-05-26 19:24:47 +0800

    2nd day: fix bugs, add players name, add scores, add reset button.

--- Tic Tac Toe/Form1.Designer.cs
+++ Tic Tac Toe/Form1.Designer.cs
@@ -41,13 +41,25 @@
             this.BtnTtt9 = new System.Windows.Forms.Button();
             this.BtnTttStart = new System.Windows.Forms.Button();
             this.BtnTttPlayer = new System.Windows.Forms.Button();
+            this.LblTttNextMove = new System.Windows.Forms.Label();
+            this.LblTttTitle = new System.Windows.Forms.Label();
+            this.LblTttP1Enn = new System.Windows.Forms.Label();
+            this.LblTttP2Enn = new System.Windows.Forms.Label();
+            this.TbTttP1Nn = new System.Windows.Forms.TextBox();
+            this.TbTttP2Nn = new System.Windows.Forms.TextBox();
+            this.LblTttScorelbl1 = new System.Windows.Forms.Label();
+            this.LblTttScorelbl2 = new System.Windows.Forms.Label();
+            this.LblTttScore1 = new System.Windows.Forms.Label();
+            this.LblTttScore2 = new System.Windows.Forms.Label();
+            this.BtnTttResetScore = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // BtnTtt1
             // 
+            this.BtnTtt1.Enabled = false;
             this.BtnTtt1.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt1.Location = new System.Drawing.Point(239, 92);
+            this.BtnTtt1.Location = new System.Drawing.Point(255, 111);
             this.BtnTtt1.Name = "BtnTtt1";
             this.BtnTtt1.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt1.TabIndex = 0;
@@ -57,8 +69,9 @@
             // 
             // BtnTtt2
             // 
+            this.BtnTtt2.Enabled = false;
             this.BtnTtt2.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt2.Location = new System.Drawing.Point(354, 92);
+            this.BtnTtt2.Location = new System.Drawing.Point(370, 111);
             this.BtnTtt2.Name = "BtnTtt2";
             this.BtnTtt2.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt2.TabIndex = 1;
@@ -68,8 +81,9 @@
             // 
             // BtnTtt3
             // 
+            this.BtnTtt3.Enabled = false;
             this.BtnTtt3.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt3.Location = new System.Drawing.Point(469, 92);
+            this.BtnTtt3.Location = new System.Drawing.Point(485, 111);
             this.BtnTtt3.Name = "BtnTtt3";
             this.BtnTtt3.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt3.TabIndex = 2;
@@ -79,8 +93,9 @@
             // 
             // BtnTtt4
             // 
+            this.BtnTtt4.Enabled = false;
             this.BtnTtt4.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt4.Location = new System.Drawing.Point(239, 190);
+            this.BtnTtt4.Location = new System.Drawing.Point(255, 209);
             this.BtnTtt4.Name = "BtnTtt4";
             this.BtnTtt4.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt4.TabIndex = 3;
@@ -93,8 +108,9 @@
             this.BtnTtt5.AllowDrop = true;
             this.BtnTtt5.CausesValidation = false;
             this.BtnTtt5.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnTtt5.Enabled = false;
             this.BtnTtt5.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt5.Location = new System.Drawing.Point(354, 190);
+            this.BtnTtt5.Location = new System.Drawing.Point(370, 209);
             this.BtnTtt5.Name = "BtnTtt5";
             this.BtnTtt5.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt5.TabIndex = 4;
@@ -104,8 +120,9 @@
             // 
             // BtnTtt6
             // 
+            this.BtnTtt6.Enabled = false;
             this.BtnTtt6.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt6.Location = new System.Drawing.Point(469, 190);
+            this.BtnTtt6.Location = new System.Drawing.Point(485, 209);
             this.BtnTtt6.Name = "BtnTtt6";
             this.BtnTtt6.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt6.TabIndex = 5;
@@ -115,8 +132,9 @@
             // 
             // BtnTtt7
             // 
+            this.BtnTtt7.Enabled = false;
             this.BtnTtt7.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt7.Location = new System.Drawing.Point(239, 288);
+            this.BtnTtt7.Location = new System.Drawing.Point(255, 307);
             this.BtnTtt7.Name = "BtnTtt7";
             this.BtnTtt7.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt7.TabIndex = 6;
@@ -126,8 +144,9 @@
             // 
             // BtnTtt8
             // 
+            this.BtnTtt8.Enabled = false;
             this.BtnTtt8.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt8.Location = new System.Drawing.Point(354, 288);
+            this.BtnTtt8.Location = new System.Drawing.Point(370, 307);
             this.BtnTtt8.Name = "BtnTtt8";
             this.BtnTtt8.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt8.TabIndex = 7;
@@ -137,8 +156,9 @@
             // 
             // BtnTtt9
             // 
+            this.BtnTtt9.Enabled = false;
             this.BtnTtt9.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTtt9.Location = new System.Drawing.Point(469, 288);
+            this.BtnTtt9.Location = new System.Drawing.Point(485, 307);
             this.BtnTtt9.Name = "BtnTtt9";
             this.BtnTtt9.Size = new System.Drawing.Size(116, 99);
             this.BtnTtt9.TabIndex = 8;
@@ -149,7 +169,7 @@
             // BtnTttStart
             // 
             this.BtnTttStart.Font = new System.Drawing.Font("Sitka Small", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
-            this.BtnTttStart.Location = new System.Drawing.Point(314, 419);
+            this.BtnTttStart.Location = new System.Drawing.Point(327, 425);
             this.BtnTttStart.Name = "BtnTttStart";
             this.BtnTttStart.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.BtnTttStart.Size = new System.Drawing.Size(201, 49);
@@ -162,7 +182,7 @@
             // 
             this.BtnTttPlayer.Enabled = false;
             this.BtnTttPlayer.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.BtnTttPlayer.Location = new System.Drawing.Point(65, 228);
+            this.BtnTttPlayer.Location = new System.Drawing.Point(59, 439);
             this.BtnTttPlayer.Name = "BtnTttPlayer";
             this.BtnTttPlayer.Size = new System.Drawing.Size(94, 44);
             this.BtnTttPlayer.TabIndex = 10;
@@ -170,11 +190,135 @@
             this.BtnTttPlayer.UseVisualStyleBackColor = true;
             this.BtnTttPlayer.Click += new System.EventHandler(this.BtnTttPlayer_Click);
             // 
+            // LblTttNextMove
+            // 
+            this.LblTttNextMove.AutoSize = true;
+            this.LblTttNextMove.Font = new System.Drawing.Font("Sitka Small", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LblTttNextMove.Location = new System.Drawing.Point(13, 410);
+            this.LblTttNextMove.Name = "LblTttNextMove";
+            this.LblTttNextMove.Size = new System.Drawing.Size(121, 26);
+            this.LblTttNextMove.TabIndex = 11;
+            this.LblTttNextMove.Text = "Next move:";
+            this.LblTttNextMove.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // LblTttTitle
+            // 
+            this.LblTttTitle.AutoSize = true;
+            this.LblTttTitle.Font = new System.Drawing.Font("Showcard Gothic", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.LblTttTitle.Location = new System.Drawing.Point(162, -3);
+            this.LblTttTitle.Name = "LblTttTitle";
+            this.LblTttTitle.Size = new System.Drawing.Size(542, 98);
+            this.LblTttTitle.TabIndex = 12;
+            this.LblTttTitle.Text = "Tic-Tac-Toe";
+            // 
+            // LblTttP1Enn
+            // 
+            this.LblTttP1Enn.AutoSize = true;
+            this.LblTttP1Enn.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.LblTttP1Enn.Location = new System.Drawing.Point(59, 114);
+            this.LblTttP1Enn.Name = "LblTttP1Enn";
+            this.LblTttP1Enn.Size = new System.Drawing.Size(139, 18);
+            this.LblTttP1Enn.TabIndex = 13;
+            this.LblTttP1Enn.Text = "Enter Nickname:";
+            this.LblTttP1Enn.Click += new System.EventHandler(this.LblTttP1Enn_Click);
+            // 
+            // LblTttP2Enn
+            // 
+            this.LblTttP2Enn.AutoSize = true;
+            this.LblTttP2Enn.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.LblTttP2Enn.Location = new System.Drawing.Point(655, 112);
+            this.LblTttP2Enn.Name = "LblTttP2Enn";
+            this.LblTttP2Enn.Size = new System.Drawing.Size(139, 18);
+            this.LblTttP2Enn.TabIndex = 14;
+            this.LblTttP2Enn.Text = "Enter Nickname:";
+            // 
+            // TbTttP1Nn
+            // 
+            this.TbTttP1Nn.Font = new System.Drawing.Font("Sitka Heading", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TbTttP1Nn.Location = new System.Drawing.Point(42, 135);
+            this.TbTttP1Nn.Name = "TbTttP1Nn";
+            this.TbTttP1Nn.Size = new System.Drawing.Size(174, 26);
+            this.TbTttP1Nn.TabIndex = 15;
+            this.TbTttP1Nn.Text = "Player 1";
+            this.TbTttP1Nn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // TbTttP2Nn
+            // 
+            this.TbTttP2Nn.Font = new System.Drawing.Font("Sitka Heading", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.TbTttP2Nn.Location = new System.Drawing.Point(638, 133);
+            this.TbTttP2Nn.Name = "TbTttP2Nn";
+            this.TbTttP2Nn.Size = new System.Drawing.Size(174, 26);
+            this.TbTttP2Nn.TabIndex = 16;
+            this.TbTttP2Nn.Text = "Player 2";
+            this.TbTttP2Nn.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LblTttScorelbl1
+            // 
+            this.LblTttScorelbl1.AutoSize = true;
+            this.LblTttScorelbl1.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblTttScorelbl1.Location = new System.Drawing.Point(100, 168);
+            this.LblTttScorelbl1.Name = "LblTttScorelbl1";
+            this.LblTttScorelbl1.Size = new System.Drawing.Size(60, 18);
+            this.LblTttScorelbl1.TabIndex = 17;
+            this.LblTttScorelbl1.Text = "Score:";
+            // 
+            // LblTttScorelbl2
+            // 
+            this.LblTttScorelbl2.AutoSize = true;
+            this.LblTttScorelbl2.Font = new System.Drawing.Font("Showcard Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblTttScorelbl2.Location = new System.Drawing.Point(696, 166);
+            this.LblTttScorelbl2.Name = "LblTttScorelbl2";
+            this.LblTttScorelbl2.Size = new System.Drawing.Size(60, 18);
+            this.LblTttScorelbl2.TabIndex = 18;
+            this.LblTttScorelbl2.Text = "Score:";
+            // 
+            // LblTttScore1
+            // 
+            this.LblTttScore1.AutoSize = true;
+            this.LblTttScore1.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblTttScore1.Location = new System.Drawing.Point(106, 187);
+            this.LblTttScore1.Name = "LblTttScore1";
+            this.LblTttScore1.Size = new System.Drawing.Size(46, 50);
+            this.LblTttScore1.TabIndex = 19;
+            this.LblTttScore1.Text = "0";
+            // 
+            // LblTttScore2
+            // 
+            this.LblTttScore2.AutoSize = true;
+            this.LblTttScore2.Font = new System.Drawing.Font("Showcard Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblTttScore2.Location = new System.Drawing.Point(702, 185);
+            this.LblTttScore2.Name = "LblTttScore2";
+            this.LblTttScore2.Size = new System.Drawing.Size(46, 50);
+            this.LblTttScore2.TabIndex = 20;
+            this.LblTttScore2.Text = "0";
+            // 
+            // BtnTttResetScore
+            // 
+            this.BtnTttResetScore.Font = new System.Drawing.Font("Sitka Small", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnTttResetScore.Location = new System.Drawing.Point(723, 410);
+            this.BtnTttResetScore.Name = "BtnTttResetScore";
+            this.BtnTttResetScore.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.BtnTttResetScore.Size = new System.Drawing.Size(98, 26);
+            this.BtnTttResetScore.TabIndex = 21;
+            this.BtnTttResetScore.Text = "Reset Score";
+            this.BtnTttResetScore.UseVisualStyleBackColor = true;
+            this.BtnTttResetScore.Click += new System.EventHandler(this.BtnTttResetScore_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(856, 495);
+            this.Controls.Add(this.BtnTttResetScore);
+            this.Controls.Add(this.LblTttScore2);
+            this.Controls.Add(this.LblTttScore1);
+            this.Controls.Add(this.LblTttScorelbl2);
+            this.Controls.Add(this.LblTttScorelbl1);
+            this.Controls.Add(this.TbTttP2Nn);
+            this.Controls.Add(this.TbTttP1Nn);
+            this.Controls.Add(this.LblTttP2Enn);
+            this.Controls.Add(this.LblTttP1Enn);
+            this.Controls.Add(this.LblTttNextMove);
             this.Controls.Add(this.BtnTttPlayer);
             this.Controls.Add(this.BtnTttStart);
             this.Controls.Add(this.BtnTtt9);
@@ -186,11 +330,14 @@
             this.Controls.Add(this.BtnTtt3);
             this.Controls.Add(this.BtnTtt2);
             this.Controls.Add(this.BtnTtt1);
+            this.Controls.Add(this.LblTttTitle);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Tic-Tac-Toe";
             this.Load += new System.EventHandler(this.Form1_Load_1);
             ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -207,5 +354,16 @@
         private Button BtnTtt9;
         private Button BtnTttStart;
         private Button BtnTttPlayer;
+        private Label LblTttNextMove;
+        private Label LblTttTitle;
+        private Label LblTttP1Enn;
+        private Label LblTttP2Enn;
+        private TextBox TbTttP1Nn;
+        private TextBox TbTttP2Nn;
+        private Label LblTttScorelbl1;
+        private Label LblTttScorelbl2;
+        private Label LblTttScore1;
+        private Label LblTttScore2;
+        private Button BtnTttResetScore;
     }
 }
--- Tic Tac Toe/Form1.cs
+++ Tic Tac Toe/Form1.cs
@@ -11,13 +11,24 @@ namespace Tic_Tac_Toe
         {
            
         }
-
+        //Initiating Button
         private void BtnTttStart_Click(object sender, EventArgs e)
         {
-           
+
             if (BtnTttStart.Text == "Start Game!" )
             {
-                BtnTttStart.Text = "Finish";
+                BtnTttStart.Text = "Finish!";
+
+                BtnTtt1.Enabled = true;
+                BtnTtt2.Enabled = true;
+                BtnTtt3.Enabled = true;
+                BtnTtt4.Enabled = true;
+                BtnTtt5.Enabled = true;
+                BtnTtt6.Enabled = true;
+                BtnTtt7.Enabled = true;
+                BtnTtt8.Enabled = true;
+                BtnTtt9.Enabled = true;
+
                 BtnTtt1.Text = "";
                 BtnTtt2.Text = "";
                 BtnTtt3.Text = "";
@@ -27,10 +38,22 @@ namespace Tic_Tac_Toe
                 BtnTtt7.Text = "";
                 BtnTtt8.Text = "";
                 BtnTtt9.Text = "";
+
             }
-            else if(BtnTttStart.Text == "Finish")
+            else if(BtnTttStart.Text == "Finish!")
             {
                 BtnTttStart.Text = "Start Game!";
+
+                BtnTtt1.Enabled = false;
+                BtnTtt2.Enabled = false;
+                BtnTtt3.Enabled = false;
+                BtnTtt4.Enabled = false;
+                BtnTtt5.Enabled = false;
+                BtnTtt6.Enabled = false;
+                BtnTtt7.Enabled = false;
+                BtnTtt8.Enabled = false;
+                BtnTtt9.Enabled = false;
+
                 BtnTtt1.Text = "T";
                 BtnTtt2.Text = "I";
                 BtnTtt3.Text = "C";
@@ -40,17 +63,21 @@ namespace Tic_Tac_Toe
                 BtnTtt7.Text = "T";
                 BtnTtt8.Text = "O";
                 BtnTtt9.Text = "E";
+
+                BtnTttPlayer.Text = "X";
+
             }
         }
         
 
-
+        //Buttons Control
         private void BtnTtt1_Click(object sender, EventArgs e)
         {
             BtnTttPlayer.Enabled = true;
             BtnTtt1.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt1.Enabled = false;
         }
 
         private void BtnTtt2_Click(object sender, EventArgs e)
@@ -59,6 +86,7 @@ namespace Tic_Tac_Toe
             BtnTtt2.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt2.Enabled = false;
         }
 
         private void BtnTtt3_Click(object sender, EventArgs e)
@@ -67,6 +95,7 @@ namespace Tic_Tac_Toe
             BtnTtt3.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt3.Enabled = false;
         }
 
         private void BtnTtt4_Click(object sender, EventArgs e)
@@ -75,6 +104,7 @@ namespace Tic_Tac_Toe
             BtnTtt4.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt4.Enabled = false;
         }
 
         private void BtnTtt5_Click(object sender, EventArgs e)
@@ -83,6 +113,7 @@ namespace Tic_Tac_Toe
             BtnTtt5.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt5.Enabled = false;
         }
 
         private void BtnTtt6_Click(object sender, EventArgs e)
@@ -91,6 +122,7 @@ namespace Tic_Tac_Toe
             BtnTtt6.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt6.Enabled = false;
         }
 
         private void BtnTtt7_Click(object sender, EventArgs e)
@@ -99,6 +131,7 @@ namespace Tic_Tac_Toe
             BtnTtt7.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt7.Enabled = false;
         }
 
         private void BtnTtt8_Click(object sender, EventArgs e)
@@ -107,6 +140,7 @@ namespace Tic_Tac_Toe
             BtnTtt8.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt8.Enabled = false;
         }
 
         private void BtnTtt9_Click(object sender, EventArgs e)
@@ -115,12 +149,13 @@ namespace Tic_Tac_Toe
             BtnTtt9.Text = BtnTttPlayer.Text;
             BtnTttPlayer.PerformClick();
             BtnTttPlayer.Enabled = false;
+            BtnTtt9.Enabled = false;
         }
 
        
         private void Form1_Load_1(object sender, EventArgs e)
         {
-
+            
         }
 
         private void BtnTttPlayer_Click(object sender, EventArgs e)
@@ -133,8 +168,88 @@ namespace Tic_Tac_Toe
             {
                 BtnTttPlayer.Text = "X";
             }
-            
+            //Winning Conditions
+            //Player 1
+            if(
+                ((BtnTtt1.Text == "X") && (BtnTtt2.Text == "X") && (BtnTtt3.Text == "X")) || //Horizontal 1st row
+                ((BtnTtt4.Text == "X") && (BtnTtt5.Text == "X") && (BtnTtt6.Text == "X")) || //Horizontal 2nd row
+                ((BtnTtt7.Text == "X") && (BtnTtt8.Text == "X") && (BtnTtt9.Text == "X")) || //Horizontal 3rd row
+                ((BtnTtt1.Text == "X") && (BtnTtt5.Text == "X") && (BtnTtt9.Text == "X")) || //Diagonal 1-5-9
+                ((BtnTtt3.Text == "X") && (BtnTtt5.Text == "X") && (BtnTtt7.Text == "X")) || //Diagonal 3-5-7
+                ((BtnTtt1.Text == "X") && (BtnTtt4.Text == "X") && (BtnTtt7.Text == "X")) || //Vertical 1st Column
+                ((BtnTtt2.Text == "X") && (BtnTtt5.Text == "X") && (BtnTtt8.Text == "X")) || //Vertical 1st Column
+                ((BtnTtt3.Text == "X") && (BtnTtt6.Text == "X") && (BtnTtt9.Text == "X"))    //Vertical 1st Column
+              )
+            {
+                MessageBox.Show(TbTttP1Nn.Text + " Wins!");
+                
+                BtnTttStart.PerformClick();
+                
+                byte initialscore1 = byte.Parse(LblTttScore1.Text);
+                byte add1 = 1;
+                int score1 = initialscore1 + add1;
+                if (byte.Parse(LblTttScore1.Text) >= 0)
+                {
+                    LblTttScore1.Text = score1.ToString();
+                }
+            }
+            //Player 2
+            else if (
+                ((BtnTtt1.Text == "O") && (BtnTtt2.Text == "O") && (BtnTtt3.Text == "O")) || //Horizontal 1st row
+                ((BtnTtt4.Text == "O") && (BtnTtt5.Text == "O") && (BtnTtt6.Text == "O")) || //Horizontal 2nd row
+                ((BtnTtt7.Text == "O") && (BtnTtt8.Text == "O") && (BtnTtt9.Text == "O")) || //Horizontal 3rd row
+                ((BtnTtt1.Text == "O") && (BtnTtt5.Text == "O") && (BtnTtt9.Text == "O")) || //Diagonal 1-5-9
+                ((BtnTtt3.Text == "O") && (BtnTtt5.Text == "O") && (BtnTtt7.Text == "O")) || //Diagonal 3-5-7
+                ((BtnTtt1.Text == "O") && (BtnTtt4.Text == "O") && (BtnTtt7.Text == "O")) || //Vertical 1st Column
+                ((BtnTtt2.Text == "O") && (BtnTtt5.Text == "O") && (BtnTtt8.Text == "O")) || //Vertical 1st Column
+                ((BtnTtt3.Text == "O") && (BtnTtt6.Text == "O") && (BtnTtt9.Text == "O"))    //Vertical 1st Column
+                    )
+            {
+                MessageBox.Show(TbTttP2Nn.Text + " Wins!");
+
+                byte Score2 = byte.Parse(LblTttScore2.Text);
+               
+                BtnTttStart.PerformClick();
+               
+                if (Score2 >= 0)
+                {
+                    byte initialscore2 = byte.Parse(LblTttScore2.Text);
+                    byte add1 = 1;
+                    int score2 = initialscore2 + add1;
+                    if (byte.Parse(LblTttScore1.Text) >= 0)
+                    {
+                        LblTttScore2.Text = score2.ToString();
+                    }
+                }
+
+                
+            }
+            //Next player to move
+            if (BtnTttPlayer.Text == "X")
+            {
+                LblTttNextMove.Text = "Next move: " + TbTttP1Nn.Text;
+            }
+            else if (BtnTttPlayer.Text == "O")
+            {
+                LblTttNextMove.Text = "Next move: " + TbTttP2Nn.Text;
+            }
         }
-       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LblTttP1Enn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnTttResetScore_Click(object sender, EventArgs e)
+        {
+            byte reset = 0;
+            LblTttScore1.Text = reset.ToString();
+            LblTttScore2.Text = reset.ToString();
+        }
     }
 }
